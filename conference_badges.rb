b542b8a2157c94f6ddb409421bcd7e0520cda64c
Tammy Lam <lamtammy1515@gmail.com>
2020-01-25 19:52:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 
 
 def batch_badge_creator(attendees)
- badges = []
+badges = []
  attendees.each do |name|
     badges << "Hello, my name is #{name}."
  end
@@ -16,11 +16,9 @@ end
 
 
 def assign_rooms(attendees)
-   room_assignments = []
-   attendees.each.with_index do |name, index|
-   room_assignments <<  "Hello, #{name}! You'll be assigned to room #{index+1}!"
+   attendees.map.with_index do |name, index|
+   "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end 
-  return room_assignments
 end 
   
   
