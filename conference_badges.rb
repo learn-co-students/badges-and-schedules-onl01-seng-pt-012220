9ee48a4e7eb1d23bbacec3edfca2d7c7836e761f
Billy Ferrara <willfira717@gmail.com>
2020-01-29 19:25:57 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+def batch_badge_creator
+counter = 1
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+names.each{|names| puts "Hello, #{names}! You'll be assigned to room #{counter}"
+counter += 1
+end
