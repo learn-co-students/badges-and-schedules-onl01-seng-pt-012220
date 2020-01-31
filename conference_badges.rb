d31602f2d89cd3563c3ae61a110ee999ce67316e
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-01-31 00:18:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+badge_maker("Arel")
+end
+
+def batch_badge_creator
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each do {|name|} 
+  counter += 1
+  puts  "Hello, #{name}!  You'll be assigned to room #{counter}!"
+end
+  
+
